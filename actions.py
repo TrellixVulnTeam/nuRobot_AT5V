8440b6e8d2e601b6acb90e654355e16f9cd503f6
Leo Arruda <lvarruda@gmail.com>
2018-06-18 10:37:01 -0400

    Prototype
    Initial prototype

--- actions.py
+++ actions.py
@@ -1,12 +1,31 @@
-from rasa_core.actions import Action
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import unicode_literals
 
+from rasa_core.actions.action import Action
+from rasa_core.events import SlotSet
 
-class ActionExample(Action):
+class ActionWeather(Action):
+	def name(self):
+		return 'action_weather'
+		
+	def run(self, dispatcher, tracker, domain):
+		from apixu.client import ApixuClient
+		api_key = '...' #your apixu key
+		client = ApixuClient(api_key)
+		
+		loc = tracker.get_slot('location')
+		current = client.getCurrentWeather(q=loc)
+		
+		country = current['location']['country']
+		city = current['location']['name']
+		condition = current['current']['condition']['text']
+		temperature_c = current['current']['temp_c']
+		humidity = current['current']['humidity']
+		wind_mph = current['current']['wind_mph']
 
-    def name(self):
-        # you can then use action_example in your stories
-        return "action_example"
+		response = """It is currently {} in {} at the moment. The temperature is {} degrees, the humidity is {}% and the wind speed is {} mph.""".format(condition, city, temperature_c, humidity, wind_mph)
+						
+		dispatcher.utter_message(response)
+		return [SlotSet('location',loc)]
 
-    def run(self, dispatcher, tracker, domain):
-        # what your action should do
-        return []
